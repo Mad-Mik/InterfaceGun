95c84e3d6380de7c9b8d46c5c8a477b6ff87b1ff
mikhail.madoyan <mikhail.madoyan@gmail.com>
2023-01-09 00:19:41 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace InterfaceGun
+{
+    interface IHaseInfo
+    {
+        void ShowInfo();
+    }
+
+    interface IWeapon
+    {
+        int Damage { get; }
+        void Fire();
+    }
+
+    abstract class Weapon : IHaseInfo, IWeapon
+    {
+        public abstract int Damage { get; }
+
+        public abstract void Fire();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{GetType().Name} Damage: {Damage}");
+        }
+    }
+
+    class Gun : Weapon
+    {
+        public override int Damage { get { return 5; } }
+
+        public override void Fire()
+        {
+            Console.WriteLine("Pif!");
+        }
+    }
+
+    class LaserGun : Weapon
+    {
+        public override int Damage { get { return 8; } }
+
+        public override void Fire()
+        {
+            Console.WriteLine("Piu!");
+        }
+    }
+
+    class Bow : Weapon
+    {
+        public override int Damage => 3;
+
+        public override void Fire()
+        {
+            Console.WriteLine("Chpunk!");
+        }
+    }
+
+    class Player
+    {
+        public void Fire(IWeapon weapon)
+        {
+            weapon.Fire();
+        }
+
+        public void CheckInfo(IHaseInfo haseInfo)
+        {
+            haseInfo.ShowInfo();
+        }
+    }
+
+    class Box : IHaseInfo
+    {
+        public void ShowInfo()
+        {
+            Console.WriteLine("Special Gun");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            Weapon[] inventory = { new Gun(), new LaserGun(), new Bow() };
+
+            foreach (var item in inventory)
+            {
+                player.CheckInfo(item);
+                player.Fire(item);
+                Console.WriteLine();
+
+            }
+            player.CheckInfo(new Box());
+            Console.ReadLine();
+        }
+    }
+}
